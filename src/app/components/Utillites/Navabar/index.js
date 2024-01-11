049530b5519358b54d163a5483424e1d484d8c48
inkanovan <inkarock18@gmail.com>
2024-01-11 18:00:13 +0700

    enhacment navbar

--- src/app/components/Utillites/Navabar/index.js
+++ src/app/components/Utillites/Navabar/index.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <header className="bg-indigo-500">
             <div className="flex md:flex-row flex-col justify-between p-4">
-                <Link href="/">CUY ANIMELIST</Link>
+                <Link href="/" className="font-bold text-white text-2xl">CUY ANIMELIST</Link>
                 <input placeholder="cari anime.." className=""/>
             </div>
         </header>
